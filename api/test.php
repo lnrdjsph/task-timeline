0300544b5fa20b09560c839c5ab086f9540d67c8
Leonard Joseph Tomalon <leonard.tomalon@metroretail.local>
2026-01-05 15:23:42 +0800

    first update for api

--- api/test.php
+++ api/test.php
@@ -12,7 +12,7 @@ if($db) {
 ?>
 ```
 
-Visit: `http://localhost/tasktimeline/api/test.php`
+Visit: `http://127.0.0.1/tasktimeline/api/test.php`
 
 ## Step 5: Test API Endpoints
 
@@ -20,17 +20,17 @@ Use your browser or a tool like Postman to test:
 
 **Get all tasks:**
 ```
-GET http://localhost/tasktimeline/api/tasks
+GET http://127.0.0.1/tasktimeline/api/tasks
 ```
 
 **Get all developers:**
 ```
-GET http://localhost/tasktimeline/api/developers
+GET http://127.0.0.1/tasktimeline/api/developers
 ```
 
 **Create a task (use Postman or cURL):**
 ```
-POST http://localhost/tasktimeline/api/tasks
+POST http://127.0.0.1/tasktimeline/api/tasks
 Body (JSON):
 {
     "task_date": "2026-01-15",

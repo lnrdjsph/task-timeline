639dde4650aead962c2fa351e2b2907b9001599b
Leonard Joseph Tomalon <leonard.tomalon@metroretail.local>
2026-01-05 17:28:52 +0800

    test 8

--- api/index.php
+++ api/index.php
@@ -10,12 +10,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit();
 }
 
-include_once 'config/database.php';
-include_once 'models/Task.php';
-include_once 'models/Developer.php';
-include_once 'models/Project.php';
-include_once 'models/Phase.php';
-include_once 'models/ProjectTask.php';
+include_once __DIR__ 'config/database.php';
+include_once __DIR__ 'models/Task.php';
+include_once __DIR__ 'models/Developer.php';
+include_once __DIR__ 'models/Project.php';
+include_once __DIR__ 'models/Phase.php';
+include_once __DIR__ 'models/ProjectTask.php';
 
 $database = new Database();
 $db = $database->getConnection();
--- api/test.php
+++ api/test.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'config/database.php';
+include_once __DIR__ 'config/database.php';
 
 $database = new Database();
 $db = $database->getConnection();
